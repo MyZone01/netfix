289ad1597e3e73d5a7d2a28e07f507328f7eb241
bakseck <seck.bakar@ugb.edu.sn>
2023-12-19 13:23:01 +0000

    add: makefile to automatize some tasks

--- bookings/views.py
+++ bookings/views.py
@@ -1,8 +1,4 @@
-from decimal import Decimal
 from django.shortcuts import redirect, render
-
-# Create your views here.
-
 
 from django.shortcuts import render
 from django.urls import reverse
@@ -11,7 +7,6 @@ from bookings.models import Bookings
 from service.models import Service
 from .forms import BookingForm
 
-
 def booking_view(request, service_id):
     service = Service.objects.get(id=service_id)
     if request.method == 'POST':
@@ -19,12 +14,9 @@ def booking_view(request, service_id):
         form = BookingForm(request.POST, instance=booking)
         if form.is_valid():
             booking = form.save(commit=False)
-            booking.user = request.user  # or however you're setting the user
+            booking.user = request.user 
             booking.price = service.price_per_hour * int(request.POST['hours'])
             booking.save()
-            # no need send any data f.e. {'user': request.user, 'history': history}
-            # because it calls profile view, instead of just rendering profile.html
-            # but you can send data , like second redirect argument in curve brackets
             return redirect(reverse('profile'))
     else:
         form = BookingForm(instance=Bookings(service=service))
